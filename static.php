1b06e3b5d11d5c4930baac9f3aebd8e02302cc1f
EhaanTech <etwordpress01@gmail.com>
2018-05-12 17:26:32 +0500

    submit question issue fixed

--- static.php
+++ static.php
@@ -15,7 +15,7 @@ if (!is_admin()) {
             'fw_ext_questionsanswers_callback', $fw_ext_instance->get_declared_URI('/static/js/fw_ext_questionsanswers_callbacks.js'), array('jquery'), '1.0', true
     );
 	
-	if (is_page_template('directory/dashboard.php')) {
+	if (is_page_template('directory/dashboard.php') || is_author() ) {
 		wp_enqueue_script('fw_ext_questionsanswers_callback');
 	}
 }
